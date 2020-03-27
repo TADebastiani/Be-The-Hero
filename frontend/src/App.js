2997cff8a73f2aa6019abadfbd1d316d49a842cc
Tiago Debastiani <debastianister@gmail.com>
2020-03-26 21:29:44 -0300

    Front-end da aplicação (correção do git)

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from "./routes";
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import './global.css';
+
+function App() {
+
+    return (
+        <Routes/>
+    );
+}
+
+export default App;
