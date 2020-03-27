43a8945597365f6ab64feafeccf2d9c6cc4b2874
Tiago Debastiani <debastianister@gmail.com>
2020-03-27 13:59:41 -0300

    Testes da aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const {errors} = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -8,7 +9,8 @@ app.use(cors());
 // Configuração pra reconhecer o BODY como JSON
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
 
 
--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,5 @@
 const connection = require('../database/connection');
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 
 module.exports = {
     async index(request, response) {
@@ -10,7 +10,8 @@ module.exports = {
 
     async create(request, response) {
         const {name, email, whatsapp, city, uf} = request.body;
-        const id = crypto.randomBytes(4).toString('HEX');
+
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
@@ -21,6 +22,6 @@ module.exports = {
             uf
         });
 
-        return response.json(id);
+        return response.json({id});
     }
 };
